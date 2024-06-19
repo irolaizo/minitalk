33f5f98129edb81d75004483c516b9b3fc069e98
irolaizo <irolaizo@student.42urduliz.com>
2024-06-19 11:18:40 +0200

    try

--- server.c
+++ server.c
@@ -2,26 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-void signal_handler(int signal)
+void signalHandler(int signal)
 {
-    printf("We get you %d\n", signal);
-    int    be_error;
-    be_error = 1;
-    if (be_error)
-        printf("You are wrong\n")
+    printf("Se recibe señal %d\n", signal)
 }
+int main (void)
+{
+    signal(SIGINT, signalHandler);
+    //manejo la señal SIGNIT (Ctrl+C)
+    while(1)
+    {
+        //Something
+    }
+    return(0);
+}
+//'signalHandler' imprime el número de señal
+// Funcion: signal(SIGINT, signalHandler) configura el manejo de la señal SIGNIT para que llame a la función 'signalHandler'.
+// El programa ejecuta un bucle hasta que se reciba la señal SIGINT
 
 int main (void)
 {
-    signal(SIGINT, (signal_handler)
-    while(1)
-    {
-    }
-    return (0);
-    pid_t pid = getpid();
-    printf("El PID del proceso actual es: %d\n", pid);
-    return 0;
     sigset_t signalSet;
-    sigemptyset
+    sigemptyset(&signalSet);
+    return(0);
 }
+// 'sigemptyset' se usa para inicializar un conjunto de señales
+// 'sigemptyset(&signalSet);' inicializa el conjunto de señales vacio
+// 
+
+
+
+
